b9a2fa0dc6349dfac92ec21e277950645e10b6ab
KilliannH <killiannhervagault@gmail.com>
2018-03-11 13:32:48 +0100

    Controllers, forms, directives, and routes are understood

--- client/app.js
+++ client/app.js
@@ -1,8 +1,29 @@
-var myApp = angular.module('myApp', [])
+var myApp = angular.module('myApp', ['ngRoute']);
 
-myApp.config(function(){
+
+myApp.config(['$routeProvider', function($routeProvider){
+    
+    $routeProvider
+        .when('/', {
+            templateUrl: 'views/recipes.html'
+        })
+        .when('/recipes/details/:id', {
+            templateUrl: 'views/recipe-details.html',
+            controller: 'appController'
+        })
+        .when('/recipes/add', {
+            templateUrl: 'views/add-recipe.html',
+            controller: 'appController'
+        })
+        .when('/recipes/edit/:id', {
+            templateUrl: 'views/edit-recipe.html',
+            controller: 'appController'
+        })
+        .otherwise({
+            redirecTo: '/'
+        });
+}]);
 //use to init
-});
 
 myApp.run(function(){
 //use during app runs
